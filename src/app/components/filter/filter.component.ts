85b1c6c43a9ada5c95e65b2ce6d8aaeb6a192be8
fjacobs <felipejacobs28@gmail.com>
2025-01-16 20:18:42 +0100

    add icons

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { bootstrapSearch } from '@ng-icons/bootstrap-icons'
+import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
-
+  icon = bootstrapSearch;
 }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,11 +2,12 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TitleIconComponent } from '../../components/title-icon/title-icon.component';
+import { FilterComponent } from '../../components/filter/filter.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TitleIconComponent],
+  imports: [TitleIconComponent, FilterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
